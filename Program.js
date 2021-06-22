3ff2355421eccd3a14a4313c3bd0dcf5e2bc1929
ces-p <cesarportillord@gmail.com>
2021-06-21 21:14:16 -0400

    2nd commit

--- Program.js
+++ Program.js
@@ -4,4 +4,22 @@ function abs(n){
     mask ^ n;  
     return (mask^n) - mask ;
 }
-console.log(abs(20));
+function isPos(x){
+    return !((x&(1<<31)) | !x);
+}
+function isNeg(x){
+    return !(!((x&(1<<31)) | !x));
+}
+function isOdd(x){
+    if((x & 1) == 0)
+        return false;
+    else
+        return true;
+}
+function isEven(x){
+    if((x & 1) == 1)
+        return false;
+    else
+        return true;
+}
+console.log(isEven(21));
